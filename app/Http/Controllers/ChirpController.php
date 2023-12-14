3e91243e3de34d2769020a75b6cb702d75884566
yulluone <joshuamuuo5@gmail.com>
2023-12-14 18:18:49 +0300

    creating chirps done

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,62 +4,76 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response;
+
 
 class ChirpController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+		* Display a listing of the resource.
+		*/
+	public function index(): Response
+	{
+		return Inertia::render('Chirps/Index', [
+			//
+		]);
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+		* Show the form for creating a new resource.
+		*/
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Chirp $chirp)
-    {
-        //
-    }
+	/**
+		* Store a newly created resource in storage.
+		*/
+	public function store(Request $request): RedirectResponse
+	{
+		//
+		$validated = $request->validate([
+			'message' => 'required|string|max:255',
+		]);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Chirp $chirp)
-    {
-        //
-    }
+		$request->user()->chirps()->create($validated);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Chirp $chirp)
-    {
-        //
-    }
+		return redirect(route('chirps.index'));
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Chirp $chirp)
-    {
-        //
-    }
+	/**
+		* Display the specified resource.
+		*/
+	public function show(Chirp $chirp)
+	{
+		//
+	}
+
+	/**
+		* Show the form for editing the specified resource.
+		*/
+	public function edit(Chirp $chirp)
+	{
+		//
+	}
+
+	/**
+		* Update the specified resource in storage.
+		*/
+	public function update(Request $request, Chirp $chirp)
+	{
+		//
+	}
+
+	/**
+		* Remove the specified resource from storage.
+		*/
+	public function destroy(Chirp $chirp)
+	{
+		//
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -29,6 +30,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::resource('chirps', ChirpController::class)
+	->only(['index', 'store'])
+	->middleware(['auth', 'verified']);
+	
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
